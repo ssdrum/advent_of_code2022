3660b5ec997c2ce47fa5f64c851e9db4c4a727c4
ssdrum <dipaololuigi95@gmail.com>
2022-12-28 10:58:54 +0100

    added day2

--- day2/d2_01.py
+++ day2/d2_01.py
@@ -2,7 +2,20 @@
 
 
 def main():
-    return 0
+    tot_score = 0
+
+    with open("input2.txt") as f:
+        for l in f:
+            tot_score += calc_score("".join(l.split()))
+
+    print(tot_score)
+
+
+def calc_score(round):
+    shapes = {"X": 1, "Y": 2, "Z": 3}
+    combs = {"AX": 3, "AY": 6, "AZ": 0, "BX": 0, "BY": 3, "BZ": 6, "CX": 6, "CY": 0, "CZ": 3}
+
+    return combs[round] + shapes[round[1]]
 
 
 if __name__ == '__main__':
