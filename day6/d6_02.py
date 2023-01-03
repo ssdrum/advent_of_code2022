878fd69a29501acc8e788ba368bd16e4bec9322e
ssdrum <dipaololuigi95@gmail.com>
2023-01-03 12:59:31 +0100

    minor refactor

--- day6/d6_02.py
+++ day6/d6_02.py
@@ -5,11 +5,13 @@ def main():
     with open("input6.txt") as f:
         data = f.readline()
 
+
+    win_size = 14
     i = 0
-    while len(set(data[i:i + 14])) != 14:
+    while len(set(data[i:i + win_size])) != win_size:
         i += 1
 
-    print(i + 14)
+    print(i + win_size)
 
     return 0
 
