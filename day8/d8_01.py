d8c58a15e2a1e95d4d7d9e110aebcb85e5318a8a
ssdrum <dipaololuigi95@gmail.com>
2023-01-06 22:49:19 +0100

    added day 8 part 1

--- day8/d8_01.py
+++ day8/d8_01.py
@@ -1,51 +1,49 @@
 #!/usr/bin/env python3
 
-global rows
-global cols
-rows = []
-cols = []
 
 def main():
-
-
+    trees = [] # all trees
+    rows = [] # all rows
+    cols = [] # all columns
 
     with open("input8.txt") as f:
         lines = f.readlines()
+        for line in lines:
+            rows.append([int(n) for n in line.rstrip()])
+            for tree in line.rstrip():
+                trees.append(int(tree))
 
-    for l in lines:
-        rows.append([int(n) for n in l.rstrip()])
-    cols = [list(i) for i in zip(*rows)]
-    
-    for r in rows:
-        print(r)
+    cols = [list(l) for l in zip(*rows)] # transpose of columns
+    # calculate length and height of grid
+    h = len(lines)
+    l = len(trees) // h
 
-    h = len(rows)
-    l = len(cols)
+    # find central trees positions 
+    centr_trees = []
+    for i, tree in enumerate(trees[l:len(trees) - l]):
+        if i % l != 0 and (i + 1) % l != 0:
+            centr_trees.append(i + l)
 
-    vis = 0
-    for i in range(1, h - 1):
-        for j in range(1, l - 1):
-            print(f"{i}, {j}, {rows[i][j]}")
+    solution = 2 * l + 2 * (h - 2) # initialise solution counting outer trees
+    for pos in centr_trees:
+        # find trees on the left and on the right of the current central tree
+        row_num = pos // l
+        row = rows[row_num]
+        left = row[:pos % l]
+        right = row[pos % l + 1:]
+        # find trees above and below of the current central tree
+        col_num = pos % l
+        col = cols[col_num]
+        above = col[:pos // l]
+        below = col[pos // l + 1:]
 
-    print(vis)
+        # find all visible trees
+        curr = trees[pos]
+        if all(t < curr for t in left) or all(t < curr for t in right) \
+        or all(t < curr for t in above) or all(t < curr for t in below):
+            solution += 1
 
-
-def is_vis(tree, row, col):
-    # check row
-    for i, t in enumerate(rows[row]):
-        print(i, t)
-
-
-
-
-
-
-
-
-
-
-
-
+    print(solution)
 
 
 if __name__ == '__main__':
